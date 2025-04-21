db6972f13c7ae2526806464ea7637dee607bd9bd
Mohammad Yusuf <mdy023@gmail.com>
2025-04-22 01:38:16 +0530

    Printing user object

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,11 +3,18 @@ import { headers } from "next/headers";
 import { betterFetch } from "@better-fetch/fetch";
 import { redirect } from "next/navigation";
 
+interface Session {
+  user: {
+    id: string;
+    name: string;
+  };
+}
+
 export default async function Home() {
   const requestHeaders = await headers();
   const cookie = requestHeaders.get("cookie") || "";
 
-  const { data: session } = await betterFetch(
+  const { data: session } = await betterFetch<Session>(
     process.env.AUTH_APP_SESSION_API_URL!,
     {
       headers: {
@@ -32,6 +39,10 @@ export default async function Home() {
           <li className="mb-2 tracking-[-.01em]">
             This is a protected page, which is only accessible by signed in
             users.
+          </li>
+          <li className="max-w-2xl">
+            <span className="font-semibold">User Object: </span>
+            <pre><code className="break-all">{JSON.stringify(session.user, null, 1)}</code></pre>
           </li>
         </ol>
 
